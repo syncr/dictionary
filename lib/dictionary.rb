0a4741ac5d8f535abb91eaf713a7f55571ddf20a
Jeremy Whitaker & Amy Vaillancourt-Sals <student@epicodus.com>
2014-07-31 10:53:53 -0700

    add a method of storing words and definitions in our dictionary.

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,6 +1,18 @@
 class Term
+  @@dictionary = {}
   def initialize(word, definition)
     @word = word
     @definition = definition
+    save
+  end
+
+  def Term.dictionary
+    @@dictionary
+  end
+
+  def save
+    @@dictionary[@word] = @definition
   end
 end
+
+
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -7,5 +7,10 @@ describe Term do
     expect(test_word).to be_an_instance_of Term
   end
 
-  # it "will return an empty hash if no words are present"
+  describe '.dictionary' do
+    it 'is populated with the word as a key and definition as a value' do
+      test_word = Term.new("trapeze","a glorified swing")
+      expect(Term.dictionary["trapeze"]).to eq 'a glorified swing'
+    end
+  end
 end
